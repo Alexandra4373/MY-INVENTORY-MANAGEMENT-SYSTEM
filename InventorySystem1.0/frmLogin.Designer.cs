f5e0155fd8e07dd9950a34f9daa5ae6e29aa5db4
Alexandra Naa Atswei Quao <77993156+Alexandra4373@users.noreply.github.com>
2022-09-01 09:13:08 +0000

    updating login details to show password

--- InventorySystem1.0/frmLogin.Designer.cs
+++ InventorySystem1.0/frmLogin.Designer.cs
@@ -42,6 +42,7 @@
             this.PictureBox1 = new System.Windows.Forms.PictureBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -150,6 +151,7 @@
             this.checkBox1.TabIndex = 34;
             this.checkBox1.Text = "Show password";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // label3
             // 
@@ -162,6 +164,18 @@
             this.label3.TabIndex = 35;
             this.label3.Text = "login";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Lavender;
+            this.label4.Location = new System.Drawing.Point(585, -6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 24);
+            this.label4.TabIndex = 36;
+            this.label4.Text = "x";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
             // frmLogin
             // 
             this.AcceptButton = this.Button1;
@@ -169,6 +183,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Olive;
             this.ClientSize = new System.Drawing.Size(600, 372);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.lbldate);
@@ -205,5 +220,6 @@
         internal System.Windows.Forms.PictureBox PictureBox1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- InventorySystem1.0/frmLogin.cs
+++ InventorySystem1.0/frmLogin.cs
@@ -70,5 +70,18 @@ namespace InventorySystem1._0
         {
 
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == false)
+                txtpass.UseSystemPasswordChar = true;
+            else
+                txtpass.UseSystemPasswordChar = false;
+        }
     }
 }
